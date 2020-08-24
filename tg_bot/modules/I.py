6d4230e9736931703c4e926e40d6e79b03b88faa
BUGHUNTER <62880706+nuhmanpk7@users.noreply.github.com>
2020-08-24 14:54:03 +0530

    Update and rename INFO.py to I.py

--- tg_bot/modules/I.py
+++ tg_bot/modules/I.py
@@ -2,12 +2,12 @@
 __help__ = """
 ◆ Bot Name : *Filter Adder*
 ◆ Creator : [bughunter0](http://t.me//bughunter0)
-◆ Credits :  @Noob_admin
+◆ Credits :  All Along the Journey
 ◆ Language : Python3
 ◆ Source Code : 👉 @bughunterbots
 ◆ Server : Heroku
 ◆ Database : SQL
-◆ Build Status : Beta
+◆ Build Status : Stable(1.0.3)
 """
-__mod_name__ = "🔺About🔺"
+__mod_name__ = "About"
 
