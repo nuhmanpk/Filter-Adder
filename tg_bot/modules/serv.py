2a2f70e6919ae992a17bcc88d7cad86fef093a48
BUGHUNTER <62880706+nuhmanpk7@users.noreply.github.com>
2020-08-25 10:59:32 +0530

    Update and rename I.py to serv.py

---
+++ tg_bot/modules/serv.py
@@ -0,0 +1,13 @@
+
+__help__ = """
+◆ Bot Name : *Filter Adder*
+◆ Creator : [BUGHUNTER](http://t.me//bughunter0)
+◆ Credits :  [Infotel Admins](https://t.me/InFoTelGroup)
+◆ Language : Python3
+◆ Source Code :  [BUGHUNTERBOTS](https://t.me/bughunterbots)
+◆ Server : AWS
+◆ Database : Amazon RDS
+◆ Build Status : Stable(1.0.4)
+"""
+__mod_name__ = "About"
+
