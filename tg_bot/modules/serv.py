7b8afe9d64b23b8cc39bb99620466d7d22d5d830
BUGHUNTER <62880706+bughunter0@users.noreply.github.com>
2020-09-06 17:18:27 +0530

    Update serv.py

--- tg_bot/modules/serv.py
+++ tg_bot/modules/serv.py
@@ -5,9 +5,9 @@ __help__ = """
 ◆ Credits :  [Infotel Admins](https://t.me/InFoTelGroup)
 ◆ Language : Python3
 ◆ Source Code :  [BUGHUNTERBOTS](https://t.me/bughunterbots)
-◆ Server : AWS
-◆ Database : Amazon RDS
-◆ Build Status : Stable(1.0.4)
+◆ Server : [AWS S3](https://aws.amazon.com/s3)
+◆ Database : [Amazon RDS](https://aws.amazon.com/rds)
+◆ Build Status : Stable(1.5)
+◆ New feature : Stop All filter added
 """
 __mod_name__ = "About"
-
